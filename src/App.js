6aa9a7b5ea1c38bf90de7c1b87913ab4e5a4828e
Yan Waipann <yanwaipann@gmail.com>
2021-09-29 19:00:13 +0630

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+
+import { useTranslation, Trans } from "react-i18next";
+
+import "./App.css";
+
+import languages from "./config/languages";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{t("greeting")}</h1>
+        <h2>{t("description.part1")}</h2>
+        <h2>
+          <Trans i18nKey="description.part2" />
+        </h2>
+        {Object.keys(languages).map((lan) => {
+          return (
+            <button key={lan} onClick={() => i18n.changeLanguage(lan)}>
+              {languages[lan].displayName}
+            </button>
+          );
+        })}
       </header>
     </div>
   );
 }
 
-export default App;
+export default function WrappedApp() {
+  return (
+    <Suspense fallback="Please wait...">
+      <App />
+    </Suspense>
+  );
+}
---
+++ src/config/languages.js
@@ -0,0 +1,7 @@
+const languages = {
+  en: { displayName: "English" },
+  mm: { displayName: "မြန်မာဘာသာ" },
+  jp: { displayName: "日本語" },
+};
+
+export default languages;
